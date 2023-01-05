4df57b79468118f6412a06be32fe9ea3ba6b5151
unknown <bayuxbca@gmail.com>
2023-01-05 10:24:00 +0700

    finalizing apps

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,10 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('api')->group(function()
-{
+Route::middleware('api')->group(function () {
     Route::resource('products', ProductController::class);
 });
